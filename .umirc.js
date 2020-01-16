24f3a1f41790988d4af970398bd1f0380c24486e
dontdrinkmylemmontea <492861128@qq.com>
2020-01-16 16:55:01 +0800

    url用hash

--- .umirc.js
+++ .umirc.js
@@ -1,30 +1,33 @@
-
 // ref: https://umijs.org/config/
+const pathName = './';
+
 export default {
   treeShaking: true,
+  base: pathName,
+  publicPath: pathName,
+  history: 'hash',
   routes: [
     {
       path: '/',
       component: '../layouts/index',
-      routes: [
-        { path: '/', component: '../pages/index' }
-      ]
-    }
+      routes: [{ path: '/', component: '../pages/index' }],
+    },
   ],
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
-    ['umi-plugin-react', {
-      antd: false,
-      dva: false,
-      dynamicImport: false,
-      title: 'examinationpaper',
-      dll: false,
-      
-      routes: {
-        exclude: [
-          /components\//,
-        ],
+    [
+      'umi-plugin-react',
+      {
+        antd: false,
+        dva: false,
+        dynamicImport: false,
+        title: 'examinationpaper',
+        dll: false,
+
+        routes: {
+          exclude: [/components\//],
+        },
       },
-    }],
+    ],
   ],
-}
+};
